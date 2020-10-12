8513bb804e0b1b04736f207d64419254963c11ca
xifeng <xifeng@dtstack.com>
2020-10-12 20:06:34 +0800

    10-12

---
+++ demo01/src/index.js
@@ -0,0 +1,20 @@
+/*
+    index.js: webpack入口文件
+
+    1. 运行命令：
+        开发环境：webpack ./src/index.js -o ./build/built.js --mode=development
+            webpack会以./src/index.js为入口文件开始打包，打包后输出到./build/built.js
+        生产环境：webpack ./src/index.js -o ./build/built.js --mode=production
+    2. 结论：
+        1. webpack能处理js/json资源，不能处理css/img等其他资源
+        2. 生产环境和开发环境将ES6模块花编译成浏览器能识别的模块化
+        3. 生产环境比开发环境多一个压缩js代码
+*/
+import data from './data.json'
+
+console.log(data)
+function add(x, y) {
+    return x + y;
+}
+
+console.log(add(1, 2));
