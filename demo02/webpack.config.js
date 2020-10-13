788b65626f24ae0c47d7e91b44557e1530e345de
xifeng <xifeng@dtstack.com>
2020-10-13 20:37:25 +0800

    10-13

---
+++ demo02/webpack.config.js
@@ -0,0 +1,37 @@
+/*
+    webpack.config.js webpack的配置文件
+        作用: 指示webpack干哪些活 
+*/
+
+const { resolve } = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'built.js',
+        path: resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            },
+            {
+                test: /.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+
+    ],
+    mode: 'development'
+}
