2b1b18e5daa07faecca2e5c2557b94d2b2a85cbb
chenrui668 <1941718132@qq.com>
2020-11-02 21:30:40 +0800

    11-2

---
+++ demo12/webpack.config.js
@@ -0,0 +1,146 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const {resolve} = require('path');
+
+/*
+    缓存： 
+        babel缓存
+            cacheDirectory: true
+        文件资源缓存
+            hash: 每次webpack构建是会生成一个唯一的hash值。
+                问题：因为js和css同时使用同一个hash值。
+                    如果重新打包，会导致所有缓存失效。（可能我却只改动了一个文件）
+            chunkhash: 根据chunk生成的hash值，如果打包来源于同一个chunk，那么hash值有一样
+                问题：js和css的hash值还是一样的
+                    因为css是在js中被引入的，所以同属于一个chunk
+            contenthash: 根据文件的内容生成hash值。不同文件hash值一定不一样
+    
+    tree shaking: 去除无用代码
+        前提：1.必须使用ES6模块化  2.开启production环境
+        作用：减少代码体积
+*/
+
+const commonCssLoader = [
+    MiniCssExtractPlugin.loader,
+    'css-loader',
+    {
+        loader: 'postcss-loader',
+        options: {
+            postcssOptions: {
+                plugins: [['postcss-preset-env',{}]]
+            }
+        }
+    }
+]
+
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'js/built.[contenthash:10].js',
+        path: resolve(__dirname, 'build')
+    },
+
+    module: {
+        rules: [
+            // 当一个文件要被多个loader处理，那么一定要指定loader的执行顺序
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                // 优先执行
+                enforce: 'pre',
+                options: {
+                    fix: true
+                }
+            },
+            {
+                // 以下loader只会匹配一个
+                // 注意：不能有两个配置处理同一类型文件
+                oneOf: [
+                    {
+                        test: /\.css$/,
+                        use: [
+                            ...commonCssLoader
+                        ]
+                    },
+                    {
+                        test: /\.less$/,
+                        use: [
+                            ...commonCssLoader,
+                            'less-loader'
+                        ]
+                    },
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        loader: 'babel-loader',
+                        options: {
+                            // 预设：指示js做怎样的兼容性处理
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        // 按需加载
+                                        useBuiltIns: 'usage',
+                                        // 指定core-js的版本
+                                        corejs: {
+                                            version: 3
+                                        },
+                                        // 指定兼容性做到哪个版本的浏览器
+                                        targets: {
+                                            chrome: '60',
+                                            firefox: '60',
+                                            ie: '9',
+                                            safari: '10',
+                                            edge: '17'
+                                        }
+                                    }
+                                ]
+                            ],
+                            cacheDirectory: true
+                        }
+                    },
+                    {
+                        test: /\.(jpg|png|gif)$/,
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8 * 1024,
+                            name: '[hash:10].[ext]',
+                            outputPath: 'imgs',
+                            esModule: false
+                        }
+                    },
+                    {
+                        test: /\.html$/,
+                        loader: 'html-loader'
+                    },
+                    {
+                        exclude: /\.(html|css|less|js|png|jpg|gif)/,
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'media'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/built.[contenthash:10].css'
+        }),
+        new OptimizeCssAssetsPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true
+            }
+        })
+    ],
+    mode: 'production'
+}
